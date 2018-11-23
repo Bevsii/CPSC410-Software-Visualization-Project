a23f4efcb867403b854b4d2d3b55d82cae4644b5
DESKTOP-H6KQM9J\Bevsii <bevaninchile@gmail.com>
2018-11-23 15:55:01 -0800

    added todo milestones

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String args[]){
-
+        // TODO: FIND THE FILE OR THROW AN ERROR OTHERWISE
+        // TODO: CREATE OUTPUT FILE
+        // TODO: STATIC ANALYSIS
+        // TODO: DYNAMIC ANALYSIS
     }
 }
