540117095aded84b6d1852e5cc690396128ead51
DESKTOP-H6KQM9J\Bevsii <bevaninchile@gmail.com>
2018-11-23 16:18:24 -0800

    Added functionality to create analysis output JSON file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String args[]){
+    private static String outputFileName = "Analysis.JSON";
+
+    public static void main(String args[]) throws IOException {
         // TODO: FIND THE FILE OR THROW AN ERROR OTHERWISE
-        // TODO: CREATE OUTPUT FILE
+
+        //Set up the path for the output file
+        String fileSeparator =  System.getProperty("file.separator");
+        String relativePath = "tmp" + fileSeparator + ".." + fileSeparator + outputFileName;
+
+        // Create new output file with the given path
+        File outputFile = new File(relativePath);
+
+        if (outputFile.createNewFile()){
+            System.out.println("Created output file");
+        } else {
+            System.out.println("Replaced output file");
+            outputFile.delete();
+            outputFile.createNewFile();
+        }
+
         // TODO: STATIC ANALYSIS
         // TODO: DYNAMIC ANALYSIS
     }
