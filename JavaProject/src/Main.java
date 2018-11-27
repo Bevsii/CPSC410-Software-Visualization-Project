593c918d915123833cce0c02fdfd605af4847558
DESKTOP-H6KQM9J\Bevsii <bevaninchile@gmail.com>
2018-11-27 13:19:45 -0800

    Added functionality to call program from command line

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -10,19 +10,23 @@ public class Main {
     // Arguments: Must be absolute path to the directory location containin python files
     public static void main(String args[]) throws Exception {
         // TODO: FIND THE FILE OR THROW AN ERROR OTHERWISE
+        String directoryPath = null;
         if(args.length != 1){
-            //throw new Exception("Error: There should only be one terminual argument");
+            throw new Exception("Error: Incorrect number of arguments. Expected arguments: Path to Python code directory");
+        }
+        else{
+            directoryPath = args[0];
         }
 
         //Set up the path for the output file
-        String fileSeparator =  System.getProperty("file.separator");
-        String parentDirectory = "/Users/rorycourt/1-UBC/04/410";
+        //String fileSeparator =  System.getProperty("file.separator");
+        //String parentDirectory = "/Users/rorycourt/1-UBC/04/410";
         //String parentDirectory = "E:";
 
         // NOTE: This is hardcoded for testing purposes only, normally we are going to get the absolute path to the python from the terminal arguments
-        String tempLocation = parentDirectory + fileSeparator + "CPSC410-Software-Visualization-Project"+ fileSeparator+ "410Python" + fileSeparator + "School";
+        //String tempLocation = parentDirectory + fileSeparator + "CPSC410-Software-Visualization-Project"+ fileSeparator+ "410Python" + fileSeparator + "School";
 
-        PythonAnaliser pythonAnaliser = new PythonAnaliser(tempLocation);
+        PythonAnaliser pythonAnaliser = new PythonAnaliser(directoryPath);
 
         // TODO: Delete all Python files starting with "LOG_" in Python410
 
